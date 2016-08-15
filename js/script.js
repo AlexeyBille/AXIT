93b7665f364e506eb8933ea3b79b18ff4c86fbc8
Alexey Bille <alexey.bille@mail.ru>
2016-08-15 21:19:05 +0700

    remake tabbing slider, add PSD

--- js/script.js
+++ js/script.js
@@ -1,25 +1,10 @@
-$(function () {
-
-    for (var i = 1; i <= 3; i++) {
-        $(".tabs-block").on('click', ".tab" + i, qwe())
-
-    }
-
-    function qwe() {
-        var a = i;
-        return function () {
-            for (var k = 1; k <= 3; k++) {
-                if (a == k) {
-                    $(".tabs-block").find(".slide:nth-child(" + (k + 1) + " )").removeClass("hidden");
-                    $(".tabs-block").find(".tab" + k).addClass("active-tab")
-                } else {
-                    $(".tabs-block").find(".slide:nth-child(" + (k + 1) + ")").addClass("hidden");
-                    $(".tabs-block").find(".tab" + k).removeClass("active-tab")
-                }
-            }
-        }
-    }
-})
 $('.parallax-window').parallax({
     imageSrc: 'img/u25_state0.png'
+});
+
+$(document).ready(function (ev) {
+    $('#custom_carousel').on('slide.bs.carousel', function (evt) {
+        $('#custom_carousel .controls li.active').removeClass('active');
+        $('#custom_carousel .controls li:eq(' + $(evt.relatedTarget).index() + ')').addClass('active');
+    })
 });
