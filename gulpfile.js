fa8858695961ce5e263548f4dd8216ec209f41c0
Alexey Bille <alexey.bille@mail.ru>
2016-08-22 15:18:31 +0700

    fix tabbed menu + parallax\header btn

--- gulpfile.js
+++ gulpfile.js
@@ -13,9 +13,9 @@ var gulp         = require('gulp'), // Подключаем Gulp
 	autoprefixer = require('gulp-autoprefixer');// Подключаем библиотеку для автоматического добавления префиксов
 
 gulp.task('sass', function(){ // Создаем таск Sass
-	return gulp.src('src/s*ss/**/*.s*ss') // Берем источник
+	return gulp.src('src/scss/**/*.scss') // Берем источник
 		.pipe(sass()) // Преобразуем Sass в CSS посредством gulp-sass
-		.pipe(autoprefixer(['last 15 versions', '> 1%', 'ie 8', 'ie 7'], { cascade: true })) // Создаем префиксы
+		.pipe(autoprefixer(['last 15 versions', '> 1%', 'ie 9'], { cascade: true })) // Создаем префиксы
 		.pipe(gulp.dest('src/css')) // Выгружаем результата в папку src/css
 		.pipe(browserSync.reload({stream: true})) // Обновляем CSS на странице при изменении
 });
